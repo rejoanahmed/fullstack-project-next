7ecd77bcc37d6e0359835e59350204aa969c758f
rejoanahmed <87889021+rejoanahmed@users.noreply.github.com>
2022-07-26 14:07:39 +0600

    tailwind navbar

--- components/higherLevelComp/navbar/NavBar.tsx
+++ components/higherLevelComp/navbar/NavBar.tsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import NavLink from "../../lowerLevelComp/navLink/NavLink";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 export interface INavItems {
   title: string;
   url: string;
@@ -15,32 +18,48 @@ export interface INavBar {
 const NavBar: React.FC<INavBar> = ({ brand, navItems }) => {
   const [menu, setMenu] = useState(false);
   const toggleMenu = () => {
-    setMenu((pr) => !pr);
+    setMenu((pr) => !!pr);
   };
+  const { route } = useRouter();
+  console.log(route);
+
+  const aciveClassName = "text-white bg-blue-700 ";
 
   return (
-    <header>
-      <div>
-        <NavLink href={"/"}>{brand}</NavLink>
-      </div>
-      <nav>
-        {navItems.map((item, id) => {
-          return (
-            <div key={id}>
-              <NavLink href={item.url}>{item.title}</NavLink>
-            </div>
-          );
-        })}
-      </nav>
-      <div>
-        <button onClick={toggleMenu}>
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link href="/" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            {brand}
+          </span>
+        </Link>
+        <button
+          data-collapse-toggle="navbar"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-0 dark:text-gray-400 dark:hover:bg-gray-700"
+          aria-controls="navbar"
+          aria-expanded="false"
+        >
+          {" "}
+          <span className="sr-only">open main menu</span>
           <MenuIcon />
         </button>
-        <button onClick={toggleMenu}>
-          <MenuOpenIcon />
-        </button>
+        <div className="hidden w-full md:block md:w-auto">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            {navItems.map(({ title, url }, id) => (
+              <li key={id}>
+                <Link
+                  href={url}
+                  className="block py-2 px-4 text-white bg-blue-700 rounded md:bg"
+                >
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
