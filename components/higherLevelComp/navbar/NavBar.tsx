66793165a75f47c4e025ca83c26d69728d1d6c24
rejoanahmed <87889021+rejoanahmed@users.noreply.github.com>
2022-07-26 14:57:42 +0600

    daefg

--- components/higherLevelComp/navbar/NavBar.tsx
+++ components/higherLevelComp/navbar/NavBar.tsx
@@ -5,6 +5,7 @@ import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import NavLink from "../../lowerLevelComp/navLink/NavLink";
 import Image from "next/image";
 import Link from "next/link";
+import CloseIcon from "@mui/icons-material/Close";
 import { useRouter } from "next/router";
 export interface INavItems {
   title: string;
@@ -16,10 +17,11 @@ export interface INavBar {
 }
 
 const NavBar: React.FC<INavBar> = ({ brand, navItems }) => {
-  const [menu, setMenu] = useState(false);
-  const toggleMenu = () => {
-    setMenu((pr) => !!pr);
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => {
+    setOpen((pr) => !pr);
   };
+  console.log(open);
   const { route } = useRouter();
 
   const aciveClassName =
@@ -40,10 +42,10 @@ const NavBar: React.FC<INavBar> = ({ brand, navItems }) => {
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-0"
           aria-controls="navbar"
           aria-expanded="false"
+          onClick={toggleOpen}
         >
-          {" "}
           <span className="sr-only">open main menu</span>
-          <MenuIcon />
+          {!open ? <MenuIcon /> : <CloseIcon />}
         </button>
         <div className="hidden w-full md:block md:w-auto">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
